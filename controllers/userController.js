d534cb61cb1a3d8e0afd06c27a5b77cb1f0ef755
lealinnea <leag0905@gmail.com>
2022-07-09 14:40:55 -0500

    user controller updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,101 @@
-const { Post } = require('../models');
+const { User } = require('../models');
 
 module.exports = {
-  getPosts(req, res) {
-    Post.find()
-      .then((posts) => res.json(posts))
-      .catch((err) => {
-        console.error({ message: err });
-        return res.status(500).json(err);
-      });
-  },
-  getSinglePost(req, res) {
-    Post.findOne({ _id: req.params.postId })
-      .then((post) =>
-        !post
-          ? res.status(404).json({ message: 'No post with that ID' })
-          : res.json(post)
-      )
-      .catch((err) => res.status(500).json(err));
+ // get all users
+ getUsers(req, res) {
+  User.find()
+    .select('-__v')
+    .then((userDataB) => {
+      res.json(userDataB);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+},
+// get single user
+getSingleUser(req, res) {
+  User.findOne({ _id: req.params.userId })
+    .select('-__v')
+    // populating all friend by user
+    .populate('friends')
+    // populating all users thoughts
+    .populate('thoughts')
+    
+    .then((userDataB) => {
+      if (!userDataB) {
+        return res.status(404).json({ message: 'User does not exhist' });
+      }
+      res.json(userDataB);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   },
   // create a new post
-  createPost(req, res) {
-    Post.create(req.body)
-      .then((post) => res.json(post))
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userDataB) => res.json(userDataB))
       .catch((err) => res.status(500).json(err));
   },
+
+// update a user by id and reset body
+updateUser(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    {
+      $set: req.body,
+    },
+    {
+      runValidators: true,
+      new: true,
+    }
+  )
+  // then return user data if not return error message
+    .then((userDataB) => {
+      if (!userDataB) {
+        return res.status(404).json({ message: 'User does not exhist' });
+      }
+      res.json(userDataB);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+},
+
+ // add friend 
+ addFriend(req, res) {
+  User.findOneAndUpdate({ _id: req.params.userId },
+     { $addToSet: { friends: req.params.friendId } },
+      { new: true })
+    .then((userDataB) => {
+      if (!userDataB) {
+        return res.status(404).json({ message: 'User does not exhist' });
+      }
+      res.json(userDataB);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+},
+// remove friend from user profile
+removeFriend(req, res) {
+  User.findOneAndUpdate({ _id: req.params.userId }, 
+    { $pull: { friends: req.params.friendId } },
+     { new: true })
+    .then((userDataB) => {
+      if (!userDataB) {
+        return res.status(404).json({ message: 'User does not exhist' });
+      }
+      res.json(userDataB);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+},
+
 };
